1f4925ff9989ef5190001edeccb71de8e69b6ea9
Kayla Cohn <kayla.cohn@salesloft.com>
2017-05-13 22:10:00 -0400

    Add weather reducer

---
+++ src/containers/weather_list.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'react-redux';
+
+export default class WeatherList extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
