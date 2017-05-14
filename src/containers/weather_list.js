79b3e477b7fe6294b118cb8a32bbb71c02c0a428
Kayla Cohn <kayla.cohn@salesloft.com>
2017-05-14 12:14:49 -0400

    Update weather list component to start showing table

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'react-redux';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
   render() {
     return (
-      <div></div>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
     );
   }
 }
+
+function mapStateToProps({ weather }) {
+  return { weather }; // { weather } === { weather: weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
